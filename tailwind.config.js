84e4ca19a7331f2001109c1bf5fc2f4ca04e49c9
Bykov <Bykov>
2024-06-01 01:13:24 +0200

    Full Mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
         'alfa-slab-one':['Alfa Slab One', 'Serif'],
         'dm-sans':["DM Sans", "sans-serif"]
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    }
   },
   plugins: [],
 }
